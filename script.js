24810fba08994427f4b0bc57ceaf43b277b0e130
Mitja Bajc <82049089+MtjaSaik@users.noreply.github.com>
2024-04-09 14:19:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,182 @@
-function draw() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    
-    //draw a circle
-    ctx.beginPath();
-    ctx.arc(75, 75, 10, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
+function drawIt() {
+    var x = 250;
+    var y = 700;
+    var dx = 0;
+    var dy = 1;
+    var WIDTH;
+    var HEIGHT;
+    var r = 10;
+    var ctx;
+    var paddlex;
+    var paddleh;
+    var paddlew;
+    var rightDown = false;
+    var leftDown = false;
+    var canvasMinX;
+    var canvasMaxX;
+    var bricks;
+    var NROWS;
+    var NCOLS;
+    var BRICKWIDTH;
+    var BRICKHEIGHT;
+    var PADDING;
+
+
+    function init() {
+        ctx = $('#canvas')[0].getContext("2d");
+        WIDTH = $("#canvas").width();
+        HEIGHT = $("#canvas").height();
+        return setInterval(draw, 10);
+    }
+
+    function circle(x, y, r) {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    function rect(x, y, w, h) {
+        ctx.beginPath();
+        ctx.rect(x, y, w, h);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    function clear() {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    }
+    //END LIBRARY CODE
+    function draw() {
+        clear();
+        circle(x, y, 10);
+        x += dx;
+        y += dy;
+    }
+
+    function init_paddle() {
+        paddlex = WIDTH / 2;
+        paddleh = 10;
+        paddlew = 75;
+    }
+
+    //nastavljanje leve in desne tipke
+    function onKeyDown(evt) {
+        if (evt.keyCode == 39)
+            rightDown = true;
+        else if (evt.keyCode == 37) leftDown = true;
+    }
+
+    function onKeyUp(evt) {
+        if (evt.keyCode == 39)
+            rightDown = false;
+        else if (evt.keyCode == 37) leftDown = false;
+    }
+    $(document).keydown(onKeyDown);
+    $(document).keyup(onKeyUp);
+
+    function init_mouse() {
+        //canvasMinX = $("#canvas").offset().left;
+        canvasMinX = $("canvas").offset().left;
+        canvasMaxX = canvasMinX + WIDTH;
+    }
+
+    function onMouseMove(evt) {
+        if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
+            paddlex = evt.pageX - canvasMinX;
+        }
+    }
+    $(document).mousemove(onMouseMove);
+
+    function initbricks() { //inicializacija opek - polnjenje v tabelo
+        NROWS = 5;
+        NCOLS = 5;
+        BRICKWIDTH = (WIDTH / NCOLS) - 1;
+        BRICKHEIGHT = 15;
+        PADDING = 1;
+        bricks = new Array(NROWS);
+        for (i = 0; i < NROWS; i++) {
+            bricks[i] = new Array(NCOLS);
+            for (j = 0; j < NCOLS; j++) {
+                bricks[i][j] = 1;
+            }
+        }
+    }
+
+
+
+    init();
+    init_paddle();
+    init_mouse();
+    initbricks();
+
+    function draw() {
+
+        clear();
+
+        circle(x, y, 10);
+
+        if (x + dx > WIDTH - r || x + dx < r)
+
+            dx = -dx;
+
+        if (y + dy > HEIGHT - r || y + dy < r)
+
+            dy = -dy;
+
+        x += dx;
+
+        y += dy;
+
+
+
+        clear();
+        circle(x, y, 10);
+        //premik ploščice levo in desno
+        if (rightDown) paddlex += 5;
+        else if (leftDown) paddlex -= 5;
+        rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
+        if (x + dx > WIDTH - (r) || x + dx < r)
+            dx = -dx;
+        if (y + dy < r)
+            dy = -dy;
+        else if (y + dy > HEIGHT - r) {
+            if (x > paddlex && x < paddlex + paddlew)
+                dy = -dy;
+            else if (y + dy > HEIGHT - r)
+                clearInterval(intervalId);
+        }
+        x += dx;
+        y += dy;
+
+        rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
+
+        //riši opeke
+        for (i = 0; i < NROWS; i++) {
+            for (j = 0; j < NCOLS; j++) {
+                if (bricks[i][j] == 1) {
+                    rect((j * (BRICKWIDTH + PADDING)) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+            }
+        }
+
+        circle(x, y, 10);
+        //premik ploščice levo in desno
+        if (rightDown) {
+            if ((paddlex + paddlew) < WIDTH) {
+                paddlex += 5;
+            } else {
+                paddlex = WIDTH - paddlew;
+            }
+        }
+        else if (leftDown) {
+            if (paddlex > 0) {
+                paddlex -= 5;
+            } else {
+                paddlex = 0;
+            }
+        }
+    }
 }
