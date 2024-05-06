a9a0ef3dafaea9c916e21ee0052670a86018ff6b
Mitja Bajc <82049089+MtjaSaik@users.noreply.github.com>
2024-05-06 22:12:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var check = true;
 document.addEventListener("DOMContentLoaded", drawIt);
 
-function starter(){
-    if(check){
+function starter() {
+    if (check) {
         check = false;
         document.getElementById("start").style.display = "none";
     }
 }
 
 function drawIt() {
-    var x = 500;
+    var x = 250;
     var y = 700;
     var dx = 0;
     var dy = 0;
     var WIDTH;
     var HEIGHT;
-    var r = 10;
+    var r = 20;
     var ctx;
     var paddlex;
     var paddleh;
@@ -38,6 +38,48 @@ function drawIt() {
     var intTimer;
     var izpisTimer;
     var start = true;
+    var num = 0;
+    var nbrick;
+    var hit = 0;
+
+    var pickaxe = new Array();
+    pickaxe[0] = new Image();
+    pickaxe[0].src = 'pictures/pickaxe1.png';
+    pickaxe[1] = new Image();
+    pickaxe[1].src = 'pictures/pickaxe2.png';
+    pickaxe[2] = new Image();
+    pickaxe[2].src = 'pictures/pickaxe3.png';
+    pickaxe[3] = new Image();
+    pickaxe[3].src = 'pictures/pickaxe4.png';
+    pickaxe[4] = new Image();
+    pickaxe[4].src = 'pictures/pickaxe5.png';
+    pickaxe[5] = new Image();
+    pickaxe[5].src = 'pictures/pickaxe6.png';
+    pickaxe[6] = new Image();
+    pickaxe[6].src = 'pictures/pickaxe7.png';
+    pickaxe[7] = new Image();
+    pickaxe[7].src = 'pictures/pickaxe8.png';
+
+    const stone = document.getElementById("stone");
+    const deepslate = document.getElementById("deepslate");
+    const deepslate_emerald = document.getElementById("deepslate_emerald");
+    const deepslate_diamond = document.getElementById("deepslate_diamond");
+    const stone_gold = document.getElementById("stone_gold");
+    const stone_iron = document.getElementById("stone_iron");
+
+    function win(){
+        clearInterval(intAnim);
+        Swal.fire({
+          title: "YOU WON",
+          text: "gg",
+          confirmButtonText: "PLAY AGAIN",
+          confirmButtonColor: "#7d9ab3",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            location.reload();
+          }
+        });
+      }
 
     function init() {
         ctx = $('#canvas')[0].getContext("2d");
@@ -59,7 +101,7 @@ function drawIt() {
     }
 
     function rect(x, y, w, h) {
-        
+
 
         ctx.beginPath();
         ctx.rect(x, y, w, h);
@@ -71,12 +113,6 @@ function drawIt() {
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
     }
     //END LIBRARY CODE
-    function draw() {
-        clear();
-        circle(x, y, 10);
-        x += dx;
-        y += dy;
-    }
 
     function init_paddle() {
         paddlex = (WIDTH / 2) - 50;
@@ -116,17 +152,63 @@ function drawIt() {
 
     function initbricks() { //inicializacija opek - polnjenje v tabelo
         NROWS = 5;
-        NCOLS = 5;
-        BRICKWIDTH = (WIDTH / NCOLS) - 1.2;
-        BRICKHEIGHT = 30;
-        PADDING = 1;
-        bricks = new Array(NROWS);
-        for (i = 0; i < NROWS; i++) {
-            bricks[i] = new Array(NCOLS);
-            for (j = 0; j < NCOLS; j++) {
-                bricks[i][j] = 1;
-            }
-        }
+        NCOLS = 10;
+        nbrick = NROWS * NCOLS;
+        BRICKWIDTH = (WIDTH / NCOLS) - 2.2;
+        BRICKHEIGHT = 48;
+        PADDING = 2;
+        /*
+            stone - 1
+            stone iron - 2
+            stone gold - 3
+            deepslate - 4
+            deepslate diamond - 5
+            deepslate emerald - 6
+        */
+
+        //    (Math.floor(Math.random() * 3) + 4)    // deepslate
+        //    (Math.floor(Math.random() * 3) + 1)    // stone
+
+        BArray = [
+            [
+                [4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4],
+                [(Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4, 4, (Math.floor(Math.random() * 3) + 4)],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, 1, 1, 1],
+                [(Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, (Math.floor(Math.random() * 3) + 1), 1],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1)],
+            ],
+            [
+                [(Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4)],
+                [4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1],
+                [1, 1, (Math.floor(Math.random() * 3) + 1), 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1],
+                [(Math.floor(Math.random() * 3) + 1), 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1],
+            ],
+            [
+                [4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4],
+                [4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1],
+                [1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1)],
+            ],
+            [
+                [4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4)],
+                [4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, (Math.floor(Math.random() * 3) + 4), 4, 4, (Math.floor(Math.random() * 3) + 4), 4],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, (Math.floor(Math.random() * 3) + 1)],
+                [1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1],
+                [(Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1],
+            ],
+            [
+                [4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4],
+                [4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, (Math.floor(Math.random() * 3) + 4), 4, 4, 4, 4],
+                [(Math.floor(Math.random() * 3) + 1), 1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1)],
+                [1, 1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, 1, 1],
+                [1, (Math.floor(Math.random() * 3) + 1), 1, 1, 1, 1, 1, 1, (Math.floor(Math.random() * 3) + 1), 1],
+            ],
+        ];
+
+        let select = Math.floor(Math.random() * 5);
+        bricks = BArray[select];
     }
 
     function timer() {
@@ -145,6 +227,10 @@ function drawIt() {
             $("#cas").html(izpisTimer);
         }
     }
+    function anim() {
+        num++;
+    }
+
     init();
     init_paddle();
     init_mouse();
@@ -153,7 +239,12 @@ function drawIt() {
     function draw() {
         clear();
 
-        circle(x, y, 10);
+        if (num > 7) {
+            num = 0;
+        }
+        ctx.drawImage(pickaxe[num], x - r, y - r, 2 * r, 2 * r)
+
+
 
         if (x + dx > WIDTH - r || x + dx < r)
 
@@ -168,12 +259,10 @@ function drawIt() {
         y += dy;
 
 
-        clear();
-        circle(x, y, 10);
         //premik ploščice levo in desno
 
 
-        if (rightDown  && shiftDown) paddlex += 6;
+        if (rightDown && shiftDown) paddlex += 6;
         else if (rightDown) paddlex += 2.5;
         else if (leftDown && shiftDown) paddlex -= 6;
         else if (leftDown) paddlex -= 2.5;
@@ -204,7 +293,37 @@ function drawIt() {
         for (i = 0; i < NROWS; i++) {
             for (j = 0; j < NCOLS; j++) {
                 if (bricks[i][j] == 1) {
-                    rect((j * (BRICKWIDTH + PADDING)) + PADDING,
+                    ctx.drawImage(stone, j * (BRICKWIDTH + PADDING) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+
+                else if (bricks[i][j] == 2) {
+                    ctx.drawImage(stone_iron, j * (BRICKWIDTH + PADDING) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+
+                else if (bricks[i][j] == 3) {
+                    ctx.drawImage(stone_gold, j * (BRICKWIDTH + PADDING) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+
+                else if (bricks[i][j] == 4) {
+                    ctx.drawImage(deepslate, j * (BRICKWIDTH + PADDING) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+
+                else if (bricks[i][j] == 5) {
+                    ctx.drawImage(deepslate_diamond, j * (BRICKWIDTH + PADDING) + PADDING,
+                        (i * (BRICKHEIGHT + PADDING)) + PADDING,
+                        BRICKWIDTH, BRICKHEIGHT);
+                }
+
+                else if (bricks[i][j] == 6) {
+                    ctx.drawImage(deepslate_emerald, j * (BRICKWIDTH + PADDING) + PADDING,
                         (i * (BRICKHEIGHT + PADDING)) + PADDING,
                         BRICKWIDTH, BRICKHEIGHT);
                 }
@@ -212,7 +331,6 @@ function drawIt() {
         }
 
 
-        circle(x, y, 10);
         //premik ploščice levo in desno
         if (rightDown) {
             if ((paddlex + paddlew) < WIDTH) {
@@ -233,12 +351,75 @@ function drawIt() {
         colwidth = BRICKWIDTH + PADDING;
         row = Math.floor(y / rowheight);
         col = Math.floor(x / colwidth);
+
         //Če smo zadeli opeko, vrni povratno kroglo in označi v tabeli, da opeke ni več
-        if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
+        if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) { //stone
             dy = -dy; bricks[row][col] = 0;
-            tocke += 1; //v primeru, da imajo opeko večjo utež lahko prištevate tudi npr. 2 ali 3; pred tem bi bilo smiselno dodati še kakšen pogoj, ki bi signaliziral mesta opek, ki imajo višjo vrednost
+            tocke += 1;
             $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
         }
+        else if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 2) { //stone iron
+            dy = -dy; bricks[row][col] = 0;
+            tocke += 2;
+            $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
+        }
+        else if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 3) { //stone gold
+            dy = -dy; bricks[row][col] = 0;
+            tocke += 3;
+            $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
+        }
+        else if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 4) { //deepslate
+            dy = -dy; bricks[row][col] = 0;
+            tocke += 2;
+            $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
+        }
+        else if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 5) { //deepslate diamond
+            dy = -dy; bricks[row][col] = 0;
+            tocke += 5;
+            $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
+        }
+        else if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 6) { //deepslate emerald
+            dy = -dy; bricks[row][col] = 0;
+            tocke += 8;
+            $("#tocke").html(tocke);
+            hit++;
+            if (hit >= nbrick) {
+                dx = 0;
+                dy = 0;
+                win();
+            }
+        }
+
         if (x + dx > WIDTH - r || x + dx < r)
             dx = -dx;
         if (y + dy < r)
@@ -251,10 +432,11 @@ function drawIt() {
             else if (y + dy > HEIGHT - r)
                 clearInterval(intervalId);
         }
-        if(check == false && aa == true){
-            dy = -2;
+        if (check == false && aa == true) {
+            dy = 2;
+            intAnim = setInterval(anim, 50);
             aa = false;
-            }
+        }
     }
 }
 var aa = true;
